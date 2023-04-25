587ae9f5bddd9c1632dd788b1c710a1f54341b03
Imanol Zapico <izapico@kiusys.com>
2023-04-25 17:46:42 -0300

    tryinh

--- app.js
+++ app.js
@@ -1,10 +1,77 @@
-const express = require("express");
+import express from 'express';
+import fetch from 'node-fetch';
+import { Telegraf } from 'telegraf'
+import cron from 'node-cron';
+
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.type('html').send(html));
+const headers = {
+  "Accept": "*/*",
+  "Accept-Encoding": "gzip, deflate, br",
+  "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+  "Cache-Control": "no-cache",
+  "Connection": "keep-alive",
+  "Content-Length": "123",
+  "content-type": "application/json",
+  "Host": "p2p.binance.com",
+  "Origin": "https://p2p.binance.com",
+  "Pragma": "no-cache",
+  "TE": "Trailers",
+  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"
+};
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const data = {
+  asset: 'USDT',
+  tradeType: 'BUY',
+  fiat: 'USD',
+  transAmount: 0,
+  order: '',
+  page: 1,
+  rows: 10,
+  filterType: 'all',
+  "payTypes": ['Wise'],
+};
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+const chatId = process.env.CHAT_ID;
+
+async function sendSalePrices() {
+  const responseData = await fetch(
+    'https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search',
+    {
+      headers,
+      method: 'POST',
+      body: JSON.stringify(data),
+    }
+  );
+
+  if (!responseData.ok)
+    throw 'bad response';
+
+  const jsonData = await responseData.json();
+
+  const salePrices = jsonData.data.slice(0, 10).map(item => item.adv.price);
+
+  const message = `Here are the first 10 sale prices:\n${salePrices.join('\n')}`;
+
+  bot.telegram.sendMessage(chatId, message);
+}
+
+// Schedule the function to run every hour
+cron.schedule('0 * * * *', () => {
+  sendSalePrices().catch(err => console.error(err));
+});
+
+// Route to handle requests
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 
 const html = `
