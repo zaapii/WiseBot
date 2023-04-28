ca0c11cc1848f47021585e549e34810d778bfdf1
Imanol Zapico <izapico@kiusys.com>
2023-04-28 13:06:15 -0300

    every hour

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ async function sendSalePrices() {
 }
 
 // Schedule the function to run every hour
-cron.schedule('0 * * * *', () => {
+cron.schedule('00 59 * * * *', () => {
   sendSalePrices().catch(err => console.error(err));
 });
 
